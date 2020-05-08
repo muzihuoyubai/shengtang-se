d1441c7f9deaa9791104e7372b8e9edde6dd55bd
李熠 <liyi@banyuan.club>
2020-05-08 10:01:14 +0800

    志愿者汉化

--- ClassExt/Polymorphism/practice/Firm/src/club/banyuan/Volunteer.java
+++ ClassExt/Polymorphism/practice/Firm/src/club/banyuan/Volunteer.java
@@ -1,16 +1,16 @@
 package club.banyuan;
 
-//******************************************************************** // Volunteer.java Author: Lewis/Loftus
-//
-// Represents a staff member that works as a volunteer. //********************************************************************
+/**
+ * 志愿人员
+ */
 public class Volunteer extends StaffMember {
 
-  //----------------------------------------------------------------- // Sets up a volunteer using the specified information. //-----------------------------------------------------------------
+  // 使用指定的信息设置志愿者。
   public Volunteer(String eName, String eAddress, String ePhone) {
     super(eName, eAddress, ePhone);
   }
 
-  //----------------------------------------------------------------- // Returns a zero pay value for this volunteer. //-----------------------------------------------------------------
+  // 志愿者的零薪为0。
   public double pay() {
     return 0.0;
   }
